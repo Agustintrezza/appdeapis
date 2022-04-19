c445adc479890a7ffe8ffb70cbff29b82d999135
Agustintrezza <agustin.trezza@gmail.com>
2022-04-19 12:23:02 -0300

    Initial Commit1

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist/app-agusher'))
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));});
+    res.sendFile(path.join(__dirname + '/dist/app-agusher/index.html'));});
 
 app.listen(process.env.PORT || 8080);
 
+console.log('El servidor esta corriendo!!')
 
-console.log('El servidor está corriendo!');
+// comment
 
 // comment // "heroku-postbuild": "ng-build --prod", "build": "ng build", "postinstall": "ng build --aot --target=production"
 //    "watch": "ng build --watch --configuration development",
